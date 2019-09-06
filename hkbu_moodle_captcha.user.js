4341d9f4d08d1c0d8300cb2f7e1a04d3d7f4138b
Francis Mak <makfc1234@gmail.com>
2019-09-07 04:31:45 +0800

    Update hkbu_moodle_captcha.user.js

--- hkbu_moodle_captcha.user.js
+++ hkbu_moodle_captcha.user.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  Autofill captcha with Tesseract
 // @author       makfc
-// @homepage     https://github.com/makfc/hkbu-moodle-captcha-solver-userscript
+// @updateURL    https://github.com/makfc/hkbu-moodle-captcha-solver-userscript/raw/master/hkbu_moodle_captcha.user.js
+// @downloadURL  https://github.com/makfc/hkbu-moodle-captcha-solver-userscript/raw/master/hkbu_moodle_captcha.user.js
 // @match        https://buelearning.hkbu.edu.hk/login/index.php
 // @connect      buelearning.hkbu.edu.hk
 // @connect      ec2-18-139-209-106.ap-southeast-1.compute.amazonaws.com
